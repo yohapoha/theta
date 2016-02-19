faadd877f1a106f760fb0dc960d97dfb92bdfa00
Alexey Polishchuk <yohapoha@gmail.com>
2016-02-19 12:17:54 +0300

    w

--- app/js/navigation.coffee
+++ app/js/navigation.coffee
@@ -6,21 +6,24 @@ navigation = {
     }
     slider: {
         elem: $(".slider")
-        widths: []
+        width: []
         outerwidth: 40
         positions: []
+        mover: (index = navigation.link.selected) ->
+            navigation.slider.elem.css 'margin-left', navigation.slider.positions[index]
+                .css 'width', navigation.slider.width[index]
     }
-    links: {
+    link: {
         elems: $(".navigation-link")
         length: 0
-        widths: []
+        width: []
         fullWidth: 0
         margin: 0
         positions: []
         hashes: []
         selected: 0
     }
-    pages: {
+    page: {
         positions: []
     }
     scroll: {
@@ -48,79 +51,80 @@ navigation = {
                     navigation.scroll.navbar.elem.removeClass "navbar-fixed-top"
                     navigation.scroll.bottom.elem.css "height", 0
                     navigation.scroll.content.elem.css "padding-top", 0
-        scrollTo: (index) ->
-            $(window).scrollTop(navigation.pages.positions[index] - navigation.panel.height)
+        scrollTo: (index = navigation.link.selected) ->
+            $(window).scrollTop(navigation.page.positions[index])
         sliderAutoscroll: {
             startPosition: 0
             endPosition: 0
-            calcPositions: ->
-                currentPage = if navigation.links.selected <= 0 then 0 else navigation.links.selected
-                navigation.scroll.sliderAutoscroll.startPosition = navigation.pages.positions[currentPage] - navigation.panel.height
-                nextPage = if navigation.links.selected >= (navigation.links.length - 1) then (navigation.links.length - 1) else (navigation.links.selected + 1)
-                navigation.scroll.sliderAutoscroll.endPosition = navigation.pages.positions[nextPage] - navigation.panel.height
+            byScrolling: true
+            calcSliderPositions: ->
+                currentPage = if navigation.link.selected <= 0 then 0 else navigation.link.selected
+                navigation.scroll.sliderAutoscroll.startPosition = navigation.page.positions[currentPage]
+                nextPage = if navigation.link.selected >= (navigation.link.length - 1) then (navigation.link.length - 1) else (navigation.link.selected + 1)
+                navigation.scroll.sliderAutoscroll.endPosition = navigation.page.positions[nextPage]
                 if currentPage == 0 then navigation.scroll.sliderAutoscroll.startPosition = 0
                 if currentPage == nextPage then navigation.scroll.sliderAutoscroll.endPosition = Infinity
+            checkSliderPosition: (scrollPosition) ->
+                if not (navigation.scroll.sliderAutoscroll.startPosition <= scrollPosition <= navigation.scroll.sliderAutoscroll.endPosition)
+                    if scrollPosition <= navigation.scroll.sliderAutoscroll.startPosition and navigation.link.selected != 0
+                        --navigation.link.selected
+                    if scrollPosition >= navigation.scroll.sliderAutoscroll.endPosition and navigation.link.selected != navigation.link.length-1
+                        ++navigation.link.selected
+                    window.location.hash = navigation.link.hashes[navigation.link.selected]
+                    navigation.scroll.sliderAutoscroll.calcSliderPositions()
+                    navigation.slider.mover()
+
+        }
     }
-    }
+    navigationInit: ->
+        navigation.panel.width = navigation.panel.elem.width()
+        navigation.link.margin = Math.floor((navigation.panel.width - navigation.link.fullWidth) / (navigation.link.length * 2))
+        navigation.link.elems.css "padding", "0 #{navigation.link.margin}px"
+        navigation.slider.positions = []
+        navigation.link.positions.map (val, pos) ->
+            navigation.slider.positions.push(navigation.link.margin + val + (navigation.link.margin * pos * 2) - navigation.slider.outerwidth / 2)
+        navigation.slider.mover()
+        navigation.page.positions = []
+        navigation.link.elems.map (pos, elem) ->
+            if pos then navigation.page.positions.push($("#{elem.hash}-page").offset().top - navigation.panel.height)
+            else navigation.page.positions.push(0)
+        navigation.scroll.sliderAutoscroll.calcSliderPositions()
 }
-navigation.links.length = navigation.links.elems.length
+navigation.link.length = navigation.link.elems.length
+navigation.link.elems.map (pos, elem) ->
+    navigation.slider.width.push elem.offsetWidth + navigation.slider.outerwidth
+    navigation.link.width.push elem.offsetWidth
+    navigation.link.hashes.push elem.hash
 
-navigation.scroll.navbarFix()
-navigation.links.elems.map (pos, elem) ->
-    navigation.slider.widths.push elem.offsetWidth + navigation.slider.outerwidth
-    navigation.links.widths.push elem.offsetWidth
-    navigation.links.hashes.push elem.hash
-    if elem.hash == window.location.hash
-        navigation.links.selected = navigation.links.hashes.indexOf(elem.hash)
-
-navigation.links.fullWidth = navigation.links.widths.reduce (sum, current) ->
-    navigation.links.positions.push sum
+navigation.link.fullWidth = navigation.link.width.reduce (sum, current) ->
+    navigation.link.positions.push sum
     sum + current;
 , 0
-sliderMove = (index = navigation.links.selected) ->
-    navigation.slider.elem.css 'margin-left', navigation.slider.positions[index]
-        .css 'width', navigation.slider.widths[index]
+navigation.link.selected = navigation.link.hashes.indexOf(window.location.hash)
+navigation.navigationInit()
 
-do linksMargin = ->
-    navigation.panel.width = navigation.panel.elem.width()
-    navigation.links.margin = Math.floor((navigation.panel.width - navigation.links.fullWidth) / (navigation.links.length * 2))
-    navigation.links.elems.css "padding", "0 #{navigation.links.margin}px"
-    navigation.slider.positions = []
-    navigation.links.positions.map (val, pos) ->
-        navigation.slider.positions.push(navigation.links.margin + val + (navigation.links.margin * pos * 2) - navigation.slider.outerwidth / 2)
-    sliderMove()
-    navigation.pages.positions = []
-    navigation.links.elems.map (pos, elem) ->
-        navigation.pages.positions.push $("#{elem.hash}-page").offset().top
+if navigation.link.selected
+    navigation.scroll.sliderAutoscroll.byScrolling = false
+    navigation.scroll.scrollTo(navigation.link.selected)
+    navigation.scroll.navbarFix()
 
 $(window).resize ->
-    linksMargin()
+    navigation.navigationInit()
 
-navigation.links.elems.mouseover ->
-    sliderMove(navigation.links.hashes.indexOf(this.hash))
+navigation.link.elems.mouseover ->
+    navigation.slider.mover(navigation.link.hashes.indexOf(this.hash))
 
-navigation.links.elems.mouseout ->
-    sliderMove()
+navigation.link.elems.mouseout ->
+    navigation.slider.mover()
 
-navigation.links.elems.click ->
-    navigation.links.selected = navigation.links.hashes.indexOf(this.hash)
-    sliderMove(navigation.links.selected)
-    navigation.scroll.scrollTo(navigation.links.selected)
-
-
-console.log navigation.scroll.s
+navigation.link.elems.click ->
+    navigation.link.selected = navigation.link.hashes.indexOf(this.hash)
+    navigation.scroll.sliderAutoscroll.byScrolling = false
+    navigation.scroll.scrollTo()
+    navigation.scroll.sliderAutoscroll.calcSliderPositions()
 
 $(window).scroll ->
     navigation.scroll.navbarFix()
-
-    scrollPos = $(window).scrollTop()
-    
-    if not (navigation.scroll.sliderAutoscroll.startPosition <= scrollPos <= navigation.scroll.sliderAutoscroll.endPosition)
-        if scrollPos < navigation.scroll.sliderAutoscroll.startPosition and navigation.links.selected != 0
-            --navigation.links.selected
-        if scrollPos > navigation.scroll.sliderAutoscroll.endPosition and navigation.links.selected != navigation.links.length-1
-            ++navigation.links.selected
-        navigation.scroll.sliderAutoscroll.calcPositions()
-        sliderMove(navigation.links.selected)
-    else
-        console.log navigation.links.hashes[navigation.links.selected]
+    if navigation.scroll.sliderAutoscroll.byScrolling
+        navigation.scroll.sliderAutoscroll.checkSliderPosition($(window).scrollTop())
+    navigation.scroll.sliderAutoscroll.byScrolling = true
--- app/js/navigation.js
+++ app/js/navigation.js
@@ -1,5 +1,5 @@
 (function() {
-  var linksMargin, navigation, sliderMove;
+  var navigation;
 
   navigation = {
     panel: {
@@ -9,21 +9,27 @@
     },
     slider: {
       elem: $(".slider"),
-      widths: [],
+      width: [],
       outerwidth: 40,
-      positions: []
+      positions: [],
+      mover: function(index) {
+        if (index == null) {
+          index = navigation.link.selected;
+        }
+        return navigation.slider.elem.css('margin-left', navigation.slider.positions[index]).css('width', navigation.slider.width[index]);
+      }
     },
-    links: {
+    link: {
       elems: $(".navigation-link"),
       length: 0,
-      widths: [],
+      width: [],
       fullWidth: 0,
       margin: 0,
       positions: [],
       hashes: [],
       selected: 0
     },
-    pages: {
+    page: {
       positions: []
     },
     scroll: {
@@ -56,104 +62,112 @@
         }
       },
       scrollTo: function(index) {
-        return $(window).scrollTop(navigation.pages.positions[index] - navigation.panel.height);
+        if (index == null) {
+          index = navigation.link.selected;
+        }
+        return $(window).scrollTop(navigation.page.positions[index]);
       },
       sliderAutoscroll: {
         startPosition: 0,
         endPosition: 0,
-        calcPositions: function() {
+        byScrolling: true,
+        calcSliderPositions: function() {
           var currentPage, nextPage;
-          currentPage = navigation.links.selected <= 0 ? 0 : navigation.links.selected;
-          navigation.scroll.sliderAutoscroll.startPosition = navigation.pages.positions[currentPage] - navigation.panel.height;
-          nextPage = navigation.links.selected >= (navigation.links.length - 1) ? navigation.links.length - 1 : navigation.links.selected + 1;
-          navigation.scroll.sliderAutoscroll.endPosition = navigation.pages.positions[nextPage] - navigation.panel.height;
+          currentPage = navigation.link.selected <= 0 ? 0 : navigation.link.selected;
+          navigation.scroll.sliderAutoscroll.startPosition = navigation.page.positions[currentPage];
+          nextPage = navigation.link.selected >= (navigation.link.length - 1) ? navigation.link.length - 1 : navigation.link.selected + 1;
+          navigation.scroll.sliderAutoscroll.endPosition = navigation.page.positions[nextPage];
           if (currentPage === 0) {
             navigation.scroll.sliderAutoscroll.startPosition = 0;
           }
           if (currentPage === nextPage) {
             return navigation.scroll.sliderAutoscroll.endPosition = Infinity;
           }
+        },
+        checkSliderPosition: function(scrollPosition) {
+          if (!((navigation.scroll.sliderAutoscroll.startPosition <= scrollPosition && scrollPosition <= navigation.scroll.sliderAutoscroll.endPosition))) {
+            if (scrollPosition <= navigation.scroll.sliderAutoscroll.startPosition && navigation.link.selected !== 0) {
+              --navigation.link.selected;
+            }
+            if (scrollPosition >= navigation.scroll.sliderAutoscroll.endPosition && navigation.link.selected !== navigation.link.length - 1) {
+              ++navigation.link.selected;
+            }
+            window.location.hash = navigation.link.hashes[navigation.link.selected];
+            navigation.scroll.sliderAutoscroll.calcSliderPositions();
+            return navigation.slider.mover();
+          }
         }
       }
+    },
+    navigationInit: function() {
+      navigation.panel.width = navigation.panel.elem.width();
+      navigation.link.margin = Math.floor((navigation.panel.width - navigation.link.fullWidth) / (navigation.link.length * 2));
+      navigation.link.elems.css("padding", "0 " + navigation.link.margin + "px");
+      navigation.slider.positions = [];
+      navigation.link.positions.map(function(val, pos) {
+        return navigation.slider.positions.push(navigation.link.margin + val + (navigation.link.margin * pos * 2) - navigation.slider.outerwidth / 2);
+      });
+      navigation.slider.mover();
+      navigation.page.positions = [];
+      navigation.link.elems.map(function(pos, elem) {
+        if (pos) {
+          return navigation.page.positions.push($(elem.hash + "-page").offset().top - navigation.panel.height);
+        } else {
+          return navigation.page.positions.push(0);
+        }
+      });
+      return navigation.scroll.sliderAutoscroll.calcSliderPositions();
     }
   };
 
-  navigation.links.length = navigation.links.elems.length;
+  navigation.link.length = navigation.link.elems.length;
 
-  navigation.scroll.navbarFix();
-
-  navigation.links.elems.map(function(pos, elem) {
-    navigation.slider.widths.push(elem.offsetWidth + navigation.slider.outerwidth);
-    navigation.links.widths.push(elem.offsetWidth);
-    navigation.links.hashes.push(elem.hash);
-    if (elem.hash === window.location.hash) {
-      return navigation.links.selected = navigation.links.hashes.indexOf(elem.hash);
-    }
+  navigation.link.elems.map(function(pos, elem) {
+    navigation.slider.width.push(elem.offsetWidth + navigation.slider.outerwidth);
+    navigation.link.width.push(elem.offsetWidth);
+    return navigation.link.hashes.push(elem.hash);
   });
 
-  navigation.links.fullWidth = navigation.links.widths.reduce(function(sum, current) {
-    navigation.links.positions.push(sum);
+  navigation.link.fullWidth = navigation.link.width.reduce(function(sum, current) {
+    navigation.link.positions.push(sum);
     return sum + current;
   }, 0);
 
-  sliderMove = function(index) {
-    if (index == null) {
-      index = navigation.links.selected;
-    }
-    return navigation.slider.elem.css('margin-left', navigation.slider.positions[index]).css('width', navigation.slider.widths[index]);
-  };
+  navigation.link.selected = navigation.link.hashes.indexOf(window.location.hash);
 
-  (linksMargin = function() {
-    navigation.panel.width = navigation.panel.elem.width();
-    navigation.links.margin = Math.floor((navigation.panel.width - navigation.links.fullWidth) / (navigation.links.length * 2));
-    navigation.links.elems.css("padding", "0 " + navigation.links.margin + "px");
-    navigation.slider.positions = [];
-    navigation.links.positions.map(function(val, pos) {
-      return navigation.slider.positions.push(navigation.links.margin + val + (navigation.links.margin * pos * 2) - navigation.slider.outerwidth / 2);
-    });
-    sliderMove();
-    navigation.pages.positions = [];
-    return navigation.links.elems.map(function(pos, elem) {
-      return navigation.pages.positions.push($(elem.hash + "-page").offset().top);
-    });
-  })();
+  navigation.navigationInit();
+
+  if (navigation.link.selected) {
+    navigation.scroll.sliderAutoscroll.byScrolling = false;
+    navigation.scroll.scrollTo(navigation.link.selected);
+    navigation.scroll.navbarFix();
+  }
 
   $(window).resize(function() {
-    return linksMargin();
+    return navigation.navigationInit();
   });
 
-  navigation.links.elems.mouseover(function() {
-    return sliderMove(navigation.links.hashes.indexOf(this.hash));
+  navigation.link.elems.mouseover(function() {
+    return navigation.slider.mover(navigation.link.hashes.indexOf(this.hash));
   });
 
-  navigation.links.elems.mouseout(function() {
-    return sliderMove();
+  navigation.link.elems.mouseout(function() {
+    return navigation.slider.mover();
   });
 
-  navigation.links.elems.click(function() {
-    navigation.links.selected = navigation.links.hashes.indexOf(this.hash);
-    sliderMove(navigation.links.selected);
-    return navigation.scroll.scrollTo(navigation.links.selected);
+  navigation.link.elems.click(function() {
+    navigation.link.selected = navigation.link.hashes.indexOf(this.hash);
+    navigation.scroll.sliderAutoscroll.byScrolling = false;
+    navigation.scroll.scrollTo();
+    return navigation.scroll.sliderAutoscroll.calcSliderPositions();
   });
-
-  console.log(navigation.scroll.s);
 
   $(window).scroll(function() {
-    var scrollPos;
     navigation.scroll.navbarFix();
-    scrollPos = $(window).scrollTop();
-    if (!((navigation.scroll.sliderAutoscroll.startPosition <= scrollPos && scrollPos <= navigation.scroll.sliderAutoscroll.endPosition))) {
-      if (scrollPos < navigation.scroll.sliderAutoscroll.startPosition && navigation.links.selected !== 0) {
-        --navigation.links.selected;
-      }
-      if (scrollPos > navigation.scroll.sliderAutoscroll.endPosition && navigation.links.selected !== navigation.links.length - 1) {
-        ++navigation.links.selected;
-      }
-      navigation.scroll.sliderAutoscroll.calcPositions();
-      return sliderMove(navigation.links.selected);
-    } else {
-      return console.log(navigation.links.hashes[navigation.links.selected]);
+    if (navigation.scroll.sliderAutoscroll.byScrolling) {
+      navigation.scroll.sliderAutoscroll.checkSliderPosition($(window).scrollTop());
     }
+    return navigation.scroll.sliderAutoscroll.byScrolling = true;
   });
 
 }).call(this);
