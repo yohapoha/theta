5d75a40423c9b9df84bdbedf6bc85a730d9c58a5
Alexey Polishchuk <yohapoha@gmail.com>
2016-03-21 17:56:10 +0300

    w

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 /// <reference path="../../typings/jquery/jquery.d.ts" />
 /// <reference path="../../typings/maskedinput/maskedinput.d.ts" />
-function centralize(element, onResize) {
+function centralizeH(element, onResize) {
     if (onResize === void 0) { onResize = false; }
     var parent = element.parent();
     element.css("margin-left", Math.floor((parent.width() - element.outerWidth()) / 2));
@@ -10,8 +10,15 @@ function centralize(element, onResize) {
         }.debounce(300));
     }
 }
+function centralizeV(element) {
+    var parent = element.parent();
+    element.css("margin-top", Math.floor((parent.height() - element.outerHeight()) / 2));
+}
 $(document).ready(function () {
-    centralize($(".button-center"));
+    centralizeH($(".top-center__logo"), true);
+    centralizeH($(".contacts"), true);
+    centralizeV($(".contacts"));
+    centralizeH($(".button-center"));
     jQuery(function ($) {
         $("#order-phone").mask("+7(999)999-99-99");
     });
--- app/js/main.ts
+++ app/js/main.ts
@@ -1,6 +1,6 @@
 /// <reference path="../../typings/jquery/jquery.d.ts" />
 /// <reference path="../../typings/maskedinput/maskedinput.d.ts" />
-function centralize(element:JQuery, onResize:boolean = false):void {
+function centralizeH(element:JQuery, onResize:boolean = false):void {
     var parent:JQuery = element.parent();
     element.css("margin-left", Math.floor((parent.width() - element.outerWidth())/2));
     if(onResize) {
@@ -9,8 +9,15 @@ function centralize(element:JQuery, onResize:boolean = false):void {
         }.debounce(300));
     }
 }
+function centralizeV(element:JQuery) {
+    var parent:JQuery = element.parent();
+    element.css("margin-top", Math.floor((parent.height() - element.outerHeight())/2));
+}
 $(document).ready(function() {
-    centralize($(".button-center"));
+    centralizeH($(".top-center__logo"), true);
+    centralizeH($(".contacts"), true);
+    centralizeV($(".contacts"));
+    centralizeH($(".button-center"));
     jQuery(function($){
         $("#order-phone").mask("+7(999)999-99-99");
     });
